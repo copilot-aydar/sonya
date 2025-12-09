051b5ffefa9d4f9767c3f0bd1a6dcf68aa2dceab
copilot-aydar <140436153+copilot-aydar@users.noreply.github.com>
2025-12-09 21:30:22 +0300

    t

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2230358351"
+    serviceWorkerVersion: "1369109543"
   }
 });
